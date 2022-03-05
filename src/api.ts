4066c87f2834073644923919325f83375439bc29
Tamzeed Rahman <tamzeedrahman@gmail.com>
2022-03-05 15:36:11 -0500

    api check

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 export const app = express();
 
+import { createStripeCheckoutSession } from './checkout';
+
 // Allows cross origin requests
 import cors from 'cors';
 
@@ -10,9 +12,25 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 // post request issue with insomnia
 
-app.post('/test', (req: Request, res: Response) => {
-  const amount = req.body.amount;
-  res.status(200).send({ with_tax: amount * 7 });
-});
 
-console.log("hi")
+
+
+  
+  /**
+   * Checkouts
+   */
+  app.post(
+    '/checkouts/',
+    runAsync(async ({ body }: Request, res: Response) => {
+      res.send(await createStripeCheckoutSession(body.line_items));
+    })
+  );
+
+  /**
+ * Catch async errors when awaiting promises 
+ */
+ function runAsync(callback: Function) {
+    return (req: Request, res: Response, next: NextFunction) => {
+      callback(req, res, next).catch(next);
+    };
+  }
